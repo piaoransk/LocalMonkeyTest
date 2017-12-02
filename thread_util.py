2448552816dce520f6ddb8b97b6a62b9af257ea8
xyh421 <xyh421@qq.com>
2017-12-02 17:11:20 +0800

    修改了一点点呢

--- thread_util.py
+++ thread_util.py
@@ -12,7 +12,8 @@
 
 '''
 
-import threading,time
+import cPickle as Pickle
+import threading,time, re
 
 def loop():
     print('thread %s is running...' % threading.current_thread().name)
@@ -36,12 +37,11 @@ def getcpuinfo():
        cpu  pid  NAME
       0.1% 2687/com.zzl.falcon.internal: 0% user + 0.1% kernel / faults: 163 minor
         0% 2796/com.zzl.falcon.internal:channel: 0% user + 0% kernel / faults: 293 minor
-
         分割的顺序
         ['3%', '2687/com.zzl.falcon.internal:', '1%', 'user', '+', '2%', 'kernel', '/', 'faults:', '2421', 'minor']
     :return:
     """
-    cmd="adb shell dumpsys cpuinfo  grep com.zzl.falcon"
+    cmd="adb shell dumpsys cpuinfo | grep com.zzl.falcon.internal"
     output=subprocess.check_output(cmd).split()
     # print output
     s_men = ".".join([x.decode() for x in output])  # 转换为string
@@ -53,6 +53,7 @@ def getcpuinfo():
     for i in range(num):
         cpu_dic[output[i*n+1]] = output[i*n]
     print cpu_dic
+    return cpu_dic
 
 def getmeminfo():
     """
@@ -81,6 +82,100 @@ C:\Users\yuhui>adb shell "dumpsys meminfo | grep com.zzl.falcon"
         if output[i]=='kB:':
             mem_dic[output[i+1]] = output[i -1]
     print mem_dic
+    return mem_dic
 
+def getdeviceinfo():
+    """
+    [ro.build.version.release]: [5.1]
+    [ro.product.model]: [Google Nexus 4 - 5.1.0 - API 22 - 768x1280]
+    [ro.product.brand]: [generic]
+    :return:
+    """
+    cmd="adb shell getprop"
+    print(cmd)
+    # output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.readlines()
+    output = subprocess.check_output(cmd).decode()
+    # print output
+    result={}
+
+    result["release"] = getvaluefromgetprop(re.findall("release].*?]", output, re.S)[0])     # 系统版本
+    result["brand_name"] = getvaluefromgetprop(re.findall("ro.product.brand].*?]", output, re.S)[0])         # 手机名
+    result["phone_model"] = getvaluefromgetprop(re.findall("ro.product.model].*?]", output, re.S)[0])         # 品牌名
+
+    print result
+    return result
+def getvaluefromgetprop(var):
+    """
+     [ro.build.version.release]: [5.1] 获取5.1
+     ro.product.model]: [Google Nexus 4 - 5.1.0 - API 22 - 768x1280]
+    :return:
+    """
+
+    res=var.split()
+    print res
+    str = "".join([x.decode() for x in res])
+    # print str
+    str1=str.split(':')[1][1:-1]
+    # print str1
+    return str1
+
+def get_battery():
+    try:
+        cmd = "adb  shell dumpsys battery"
+        print(cmd)
+        output = subprocess.check_output(cmd).split()
+        # _batter = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
+        #                            stderr=subprocess.PIPE).stdout.readlines()
+        st = ".".join([x.decode() for x in output])  # 转换为string
+        print(st)
+        battery2 = int(re.findall("level:.(\d+)*", st, re.S)[0])
+        print battery2
+    except:
+        battery2 = 90
+    # writeInfo(battery2, PATH("../info/" + devices + "_battery.pickle"))
+
+    return battery2
+# get_battery()
+# getdeviceinfo()
 # getcpuinfo()
 # getmeminfo()
+import pickle
+path="pickle.pkl"
+s=getcpuinfo()
+f=open(path, 'a+')
+data1= Pickle.dump(s, f)
+# print data1
+with open(path, 'a+') as f:
+    data2= pickle.load(f)
+print data2.items()
+print data2.keys()
+
+def readInfo(path):
+    data = []
+    with open(path, 'a+') as f:
+        try:
+            data = pickle.load(f)
+            print data
+            # print(data)
+        except EOFError:
+            data = []
+            print("读取文件错误")
+    print("------read-------")
+    print(path)
+    print(data)
+    return data
+
+devices=['192.168.179.101:5555', '1234567']
+_app = {}
+devices_Pool=[]
+for item in range(0, len(devices)):
+    if ':' in devices[item]:
+        print '替换 :'
+        de=devices[item].replace(':', ' ')  # device 包含':' ,无法写入文件名,因为文件名不能有特殊符号
+        print 'de:',de
+        _app["devices"+str(item)] = de
+    else:
+        _app["devices"+str(item)] = devices[item]
+    _app["num"] = len(devices)
+    devices_Pool.append(_app)
+print devices_Pool
